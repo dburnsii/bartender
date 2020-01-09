6f05482d21a4751cb145e37673340971f6be95e0
Desone Burns II <dburnsii@live.com>
2020-01-09 08:58:10 -0700

    Completed initial drink creation

--- drinks/static/js/new_drink.js
+++ drinks/static/js/new_drink.js
@@ -1,49 +1,13 @@
 $(document).ready( () => {
-    /*var select = document.getElementById("id_type");
-    var option = document.createElement("option");
-    var submit =  $('input[type="submit"]');
-    option.text = "New...";
-    option.value = "new";
-    select.add(option);
-
-    select.addEventListener("change", function() {
-        if(select.value == "new")
-        {
-            console.log("New!");
-            submit.attr("disabled", true);
-            $("#categoryModal").modal();
-        }
-        else
-        {
-            console.log("Not new");
-            submit.removeAttr("disabled");
-        }
-    });*/
+    $("#colorPicker").change((e) => {
+        $(".drink-header").css("background-image", `linear-gradient(${e.target.value}, white)`);
+    });
 
     $("#colorPicker").spectrum({
-        color: "#00aeef",
+        color: "#005aff",
         preferredFormat: "hex"
     });
 
-    /*$(".ingredient-name").keyup((event) => {
-        if(suggestions.list.items.length == 0){
-            var temp = document.createElement('li');
-            temp.appendChild(document.createTextNode('New...'));
-            temp.addEventListener('click', () => {console.log("test")});
-            suggestions.list.element.appendChild(temp);
-            suggestions.list.element.style.display = "block";
-            $(".suggestions li").click((e) => {
-                console.log("test");
-            });
-        }
-
-    });
-
-    $(".suggestions").click((event) => {
-        console.log("Click");
-        console.log(event.target);
-    })*/
-
-
-
+    $("#colorPicker").spectrum("set", "#005aff");
+    $("#colorPicker").change();
 });
