d712f3dc2479df5b9a2c47a7a62eb28493f2767c
Desone Burns II <dburnsii@live.com>
2020-01-31 09:10:07 -0700

    Create Fixtures and remove location from pump

--- barkeep/models.py
+++ barkeep/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Pump(models.Model):
-    location = models.PositiveSmallIntegerField()
     address = models.PositiveSmallIntegerField()
     contents = models.ForeignKey('drinks.Ingredient', on_delete=models.CASCADE, null=True, blank=True)
 
