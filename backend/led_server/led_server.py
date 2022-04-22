0fc24f8e636ea93d76eca4f4b7dba1db031c2eb2
Desone Burns II <dburnsii@live.com>
2022-04-22 14:06:01 -0700

    Run LEDs directly

--- backend/led_server/led_server.py
+++ backend/led_server/led_server.py
@@ -1,77 +1,146 @@
 #!/usr/bin/python3
 
-import serial
 import time
 import json
-import serial.tools.list_ports
 import socketio
 import atexit
 import sys
+import math
 from datetime import datetime, timedelta
+from enum import Enum
 
-serial_queue = []
+class Mode(Enum):
+    IDLE = 1
+    HIGHLIGHT = 2
 
+# Disable this to avoid printing pixels to the terminal
+display_simulation = True
+
+led_max = 255
 simulation = True
 cup_presence_status = False
-serial_ready = False
+pixels = None
+current_mode = Mode.IDLE
+idle_index = 0
+idle_direction = 1
+
+pixel_count = 60
+pixel_range = led_max * pixel_count
+pixel_fps = 30
+
+RED = (led_max, 0, 0)
+GREEN = (0, led_max, 0)
+BLUE = (0, 0, led_max)
+WHITE = (led_max, led_max, led_max)
+
+idle_color = WHITE
 
 sio = socketio.Client()
 
 
 @atexit.register
 def cleanup():
-    if not simulation:
-        tty.close()
+    pass
 
+def simulate_pixels():
+    global pixels
+    print("\r", end="")
+    for pixel in pixels:
+        print("\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(
+                                                                    int(pixel[0]),
+                                                                    int(pixel[1]),
+                                                                    int(pixel[2]),
+                                                                    "O"),
+                                                                end='',
+                                                                sep='',
+                                                                flush=True)
 
-def serial_write(data):
-    if isinstance(data, dict):
-        data = json.dumps(data)
-    if not simulation and serial_ready:
-        tty.write((data + "\x00").encode())
+def rgb_multiply(color, intensity=1):
+    return tuple(map(lambda x: x * max(intensity, 0), color))
+
+def show_pixels():
+    global pixels
+    global simulation
+    global display_simulation
+    if simulation and display_simulation:
+        simulate_pixels()
     else:
-        print("Writing: {}".format(data))
+        pixels.show()
+
+def highlight(index, spread=10):
+    global led_max
+    global pixels
+    global pixel_count
+
+    pixels[0:pixel_count] = [(0,0,0) for i in range(pixel_count)]
+
+    spread_range_upper = (led_max * spread / 2)
+    spread_range_lower = (led_max * spread / 2)
+    lower = 0
+    upper = 0
+    if index % led_max:
+        lower = math.floor(index / led_max)
+        upper = math.ceil(index / led_max)
+        spread_range_upper -= ( ( upper * led_max) - index)
+        spread_range_lower -= ( index - ( lower * led_max))
+    else:
+        lower = math.floor(index / led_max - 1)
+        upper = math.floor(index / led_max + 1)
+        pixels[lower + 1] = rgb_multiply(idle_color, 1)
+        spread_range_upper -= led_max
+        spread_range_lower -= led_max
+    while spread_range_upper > 0 or spread_range_lower > 0:
+        if lower >= 0:
+            intensity = min(spread_range_lower, led_max) / led_max
+            pixels[lower] = rgb_multiply(idle_color, intensity)
+            lower -= 1
+        spread_range_lower -= led_max
+        if upper < pixel_count:
+            intensity = min(spread_range_upper, led_max) / led_max
+            pixels[upper] = rgb_multiply(idle_color, intensity)
+            upper += 1
+        spread_range_upper -= led_max
+
+def update_idle(increment=50):
+    global simulation
+    global pixels
+    global pixel_count
+    global idle_index
+    global idle_direction
+    idle_index += idle_direction * increment
+    if idle_index >= pixel_range:
+        idle_index = pixel_range - led_max
+        idle_direction *= -1
+    elif idle_index < 0:
+        idle_index = 0
+        idle_direction *= -1
+    highlight(idle_index)
+
 
 
 @sio.event
 def manual_pour_status(data):
-    global serial_queue
     if(data['complete']):
         idle(None)
     elif data["percentage"] >= 0 and data["percentage"] <= 100:
-        serial_queue.append({"command": "drinkProgress",
-                            "progress": data["percentage"] * 100})
-
+        # TODO: Enclosing animation
+        pass
 
 @sio.event
 def activate_valve(data):
-    global serial_queue
     global cup_presence_status
     if not simulation:
         if cup_presence_status:
             print("Activating leds above valve {}".format(data['pin']))
-            if("colors" in data):
-                serial_queue.append({"command": "highlight",
-                                     "locations": [data['pin']],
-                                     "colors": data['colors']})
-            else:
-                serial_queue.append({"command": "highlight",
-                                     "locations": [data['pin']]})
+            # TODO: Highlight bottle
     else:
         print("Simulating LED activation on valve {}".format(data['pin']))
 
 
 @sio.event
 def highlight_bottles(data):
-    global serial_queue
     print("Highlighting leds above valve {}".format(data['pins']))
-    if("colors" in data):
-        serial_queue.append({"command": "highlight",
-                             "locations": data['pins'],
-                             "colors": data['colors']})
-    else:
-        serial_queue.append({"command": "highlight",
-                             "locations": data['pins']})
+    # TODO highlight bottle
 
 
 @sio.event
@@ -92,8 +161,8 @@ def cup_presence(data):
 
 @sio.event
 def idle(data):
-    global serial_queue
-    serial_queue.append({"command": "idle"})
+    global current_mode
+    current_mode = Mode.IDLE
 
 
 @sio.event
@@ -105,27 +174,20 @@ def connect():
 @sio.event
 def simulation(data):
     global simulation
-    global serial_ready
+    global pixels
+    global pixel_count
+    global display_simulation
     print("Updating simulation status.")
     simulation = data["status"]
 
-    if not simulation:
-        global tty
-        ports = serial.tools.list_ports.comports()
-        arduino_port = ""
-        for port in ports:
-            if "Arduino" in port[1]:
-                print("Found arduino on port {}".format(port[0]))
-                arduino_port = port[0]
-                break
-        if not arduino_port:
-            print("No Arduino found")
-            return
-        tty = serial.Serial(arduino_port, 115200, timeout=2)
-        # Allow Arduino to reset due to new connection
-        time.sleep(2)
-        serial_ready = True
-        print("Serial port ready!")
+    if simulation:
+        pixels = [(0,0,0) for i in range(pixel_count)]
+        if display_simulation:
+            print("o", end="")
+    else:
+        import board
+        import neopixel
+        pixels = neopixel.NeoPixel(board.D18, pixel_count, auto_write=False)
 
 
 @sio.event
@@ -137,12 +199,15 @@ def disconnect():
 sio.connect("http://localhost:8080")
 
 lastping = datetime.now()
+lastshow = datetime.now()
 
 while 1:
     time.sleep(0.01)
-    if(serial_queue):
-        serial_write(serial_queue[-1])
-        serial_queue = []
     if(datetime.now() - lastping > timedelta(seconds=5)):
         lastping = datetime.now()
         sio.emit('ping', "")
+    if(datetime.now() - lastshow > timedelta(seconds=(1/pixel_fps))):
+        lastshow = datetime.now()
+        if current_mode == Mode.IDLE:
+            update_idle()
+        show_pixels()
---
+++ backend/led_server/led_utils.py
@@ -0,0 +1,2 @@
+def show_progress(pixels, percentage):
+    pass
